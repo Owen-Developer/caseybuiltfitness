2fa9460f5d3c49ad0093406341b4d4faa3b0d04d
Owen Marceau <marceauowen@gmail.com>
2026-01-25 12:23:32 +0000

    87segh67

--- script.js
+++ script.js
@@ -38,3 +38,34 @@ document.querySelectorAll(".pro-wrapper").forEach(wrapper => {
         }, 300);
     });
 });
+
+document.querySelector(".pro-letter-btn").addEventListener("click", () => {
+    async function saveEmail(){
+        const dataToSend = { email: document.getElementById("letterEmail").value };
+        try {
+            const response = await fetch(url + `https://servers.nextdesignwebsite.com/casey/api/save-email`, {
+                method: 'POST',
+                headers: { 
+                    'Content-Type': 'application/json', 
+                },
+                body: JSON.stringify(dataToSend), 
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                console.error('Error:', errorData.message);
+                return;
+            }
+
+            const data = await response.json();
+            if(data.message == "success"){
+                document.getElementById("letterEmail").value = "";
+            }
+        } catch (error) {
+            console.error('Error posting data:', error);
+        }
+    }
+    if(document.getElementById("letterEmail").value != ""){
+        saveEmail();
+    }
+});
